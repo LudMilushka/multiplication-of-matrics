2a2b8f089b47690dfc3f176d9b9326c5321481af
Ludmila Lapina <milalapina13@gmail.com>
2015-12-19 17:10:23 +0700

    second version - simple multiplication of two matrices(without method Vinograd)

--- MatrixMul/MatrixMul/main.cpp
+++ MatrixMul/MatrixMul/main.cpp
@@ -4,89 +4,94 @@
 using namespace std;
 int main()
 {
-    int raz1,raz2,dva1,dva2;
-    int i,j,k;
-    cout<<"Vvedite [x][y] pervoi matricy:"; cin>>raz1>>raz2;
-    cout<<"Vvedite [x][y] vtoroy matricy:"; cin>>dva1>>dva2;
-    int **matrix1 = new int*[raz1*2];   
-    for(i=1; i<=raz1;i++) 
-    { 
-    matrix1[i]=new int[raz2*2]; 
-    } 
-    
-    int **matrix2 = new int*[dva1*2]; 
-    for(i=1; i<=dva1;i++) 
-    { 
-    matrix2[i]=new int[dva2*2]; 
-    }
-    
-    int *rowFactor = new int[dva2*2];
-    int *columnFactor = new int[dva2*2];
+    int a[10][10], b[10][10],c[10][10];
+    int x,y,i,j,m,n;
  
-    int **R = new int*[raz1*2];
-    for (i=1; i<=dva1; i++)
+    cout<<"\nEnter the number of rows and columns for Matrix A:::\n\n";
+    cin>>x>>y;
+    cout<<"\n\nEnter elements for Matrix A :::\n\n";
+ 
+    for(i=0;i<x;i++)
     {
-        R[i]=new int[dva1*2];
-    }
-    
-    for (i=1; i<=raz1; i++)
-    {
-    for (j=1; j<=raz2; j++)
-    {
-        cout<<"Matrix1["<<i<<"]["<<j<<"]="; cin>>matrix1[i][j];
-    }
-    }
-   
-    for (i=1; i<=dva1; i++)
-    {
-    for (j=1; j<=dva2; j++)
-    {
-        cout<<"Matrix2["<<i<<"]["<<j<<"]="; cin>>matrix2[i][j];
-    }
-    }
-   
-    dva2=raz2/2;
-    for (i = 1; i<=raz1; i++){
-    rowFactor[i] = matrix1[i][1]*matrix1[i][2];
-    for (j = 1; j<=dva2; j++){
-    rowFactor[i] = rowFactor[i] + matrix1[i][2*j-1] * matrix1[i][2*j];
-    }
-	}
-    for (i = 1; i<=dva1; i++){
-    columnFactor[i] = matrix2[1][i]*matrix2[2][i];
-    for (j = 1; j<=dva2; j++){
-    columnFactor[i] = columnFactor[i] + matrix2[2*j-1][i] * matrix2[2*j][i];
-    }
-	}
-   
-   for (i=1; i<=raz1; i++)
-    {
-        for (j=1; j<=dva1; j++)
+        for(j=0;j<y;j++)
         {
-        R[i][j]=-rowFactor[i]-columnFactor[j];
-            for (k=1; k<=dva2; k++)
+            cin>>a[i][j];
+        }
+        cout<<"\n";
+    }
+
+	cout<<"\n\nMatrix A :\n\n";
+ 
+    for(i=0;i<x;i++)
+    {
+        for(j=0;j<y;j++)
+        {
+            cout<<"\t"<<a[i][j];
+        }
+        cout<<"\n\n";
+    }
+ 
+    cout<<"\n-----------------------------------------------------------\n";
+ 
+    cout<<"\nEnter the number of rows and columns for Matrix B:::\n\n";
+    cin>>m>>n;
+    
+    cout<<"\n\nEnter elements for Matrix B :::\n\n";
+ 
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cin>>b[i][j];
+        }
+        cout<<"\n";
+    }
+ 
+ 
+    cout<<"\n\nMatrix B :\n\n";
+ 
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cout<<"\t"<<b[i][j];
+        }
+        cout<<"\n\n";
+    }
+ 
+    if(y==m)
+    {
+ 
+        for(i=0;i<x;i++)
+        {
+            for(j=0;j<n;j++)
             {
-                R[i][j]=R[i][j]+(matrix1[i][2*k-1]+matrix2[2*k][j])*(matrix1[i][2*k]+matrix2[2*k-1][j]);
+                c[i][j]=0;
+                for(int k=0;k<m;k++)
+                {
+                    c[i][j]=c[i][j]+a[i][k]*b[k][j];
+                }
             }
         }
-    }
-    if (2*(raz2/2)==raz2/2) { 
-    for (i=1; i<=raz1; i++)
-    {
-        for (j=1; j<=dva1; j++)
+ 
+        cout<<"\n-----------------------------------------------------------\n";
+ 
+        cout<<"\n\nMultiplication of Matrix A and Matrix B :\n\n";
+ 
+        for(i=0;i<x;i++)
         {
-            R[i][j]=R[i][j]+matrix1[i][raz2]*matrix2[raz2][j];
+            for(j=0;j<n;j++)
+            {
+                cout<<"\t"<<c[i][j];
+            }
+            cout<<"\n\n";
         }
     }
-    }
-    
-    for (i=1; i<=raz1; i++)
+    else
     {
-        for (j=1; j<=dva1; j++)
-        {
-            cout<<R[i][j]<<" ";
-        }
-        cout<<endl;
+        cout<<"\n\nMultiplication is not possible";
     }
-	getch();
+ 
+    getch();
+    return 0;
 }
